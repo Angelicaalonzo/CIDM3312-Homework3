885bbd717cc070fb4595a6527c3ad39ed9a4a4c8
Angelicaalonzo <31522129+Angelicaalonzo@users.noreply.github.com>
2019-02-21 14:17:48 -0600

    now

--- Model.cs
+++ Model.cs
@@ -24,6 +24,8 @@ public class User
         public string LastName {get; set;}
         public string Email {get; set;}
         public DateTime RegistrationDate {get; set;}
+        public int AnswerID {get; set; }
+         List <User> Users {get; set; }
         public List<Question> Question {get; set;} 
        public List<Answer> Answer {get; set;}
 
@@ -39,6 +41,8 @@ public class User
         public int FirstName{get; set;} //FK
         public string QuestionText{get; set;}
         public DateTime QuestionPosted {get; set; }
+        public User User {get; set;}
+        public int AnswerID{get; set; }
     
        public List <Answer> Answers {get; set;}// One Question can have many answers
 
@@ -53,6 +57,8 @@ public class User
         public int UserID {get; set;}// FK
         public string AnswerText{get; set; }
         public DateTime AnswerPosted{get; set; }
+        public User User {get; set; }
+        public int QuestionID{get; set; }
         public Question Question {get; set; }// Each answer is associated with one question
 
 
